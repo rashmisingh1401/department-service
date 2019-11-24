abce387b91f3cf51beac31b0061aa55ac3f3d621
rashmisingh <rashmisingh@in.ibm.com>
2019-11-24 18:10:21 +0530

    new changes

--- src/main/java/pl/piomin/services/department/model/Employee.java
+++ src/main/java/pl/piomin/services/department/model/Employee.java
@@ -3,18 +3,42 @@ package pl.piomin.services.department.model;
 public class Employee {
 
 	private Long id;
+	private Long organizationId;
+	private Long departmentId;
 	private String name;
 	private int age;
 	private String position;
+	private String month;
+	private String eventName;
+
+	public String getEventName() {
+		return eventName;
+	}
+
+	public void setEventName(String eventName) {
+		this.eventName = eventName;
+	}
 
 	public Employee() {
 
 	}
 	
-	public Employee(String name, int age, String position) {
+	public Employee(Long organizationId, Long departmentId, String name, int age, String position, String month, String eventName) {
+		this.organizationId = organizationId;
+		this.departmentId = departmentId;
 		this.name = name;
 		this.age = age;
 		this.position = position;
+		this.month = month;
+		this.eventName = eventName;
+	}
+
+	public String getMonth() {
+		return month;
+	}
+
+	public void setMonth(String month) {
+		this.month = month;
 	}
 
 	public Long getId() {
@@ -23,6 +47,22 @@ public class Employee {
 
 	public void setId(Long id) {
 		this.id = id;
+	}
+
+	public Long getOrganizationId() {
+		return organizationId;
+	}
+
+	public void setOrganizationId(Long organizationId) {
+		this.organizationId = organizationId;
+	}
+
+	public Long getDepartmentId() {
+		return departmentId;
+	}
+
+	public void setDepartmentId(Long departmentId) {
+		this.departmentId = departmentId;
 	}
 
 	public String getName() {
@@ -51,7 +91,8 @@ public class Employee {
 
 	@Override
 	public String toString() {
-		return "Employee [id=" + id + ", name=" + name + ", position=" + position + "]";
+		return "Employee [id=" + id + ", organizationId=" + organizationId + ", departmentId=" + departmentId
+				+ ", name=" + name + ", position=" + position + ", month=" + month + ", eventName=" + eventName + "]";
 	}
 
 }
